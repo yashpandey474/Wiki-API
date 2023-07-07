27baef2ebd4767dfe9b765b831ebc560d0fb991d
yashpandey474 <ypandey474@gmail.com>
2023-07-08 01:31:15 +0530

    Add post, get  and delete requests

--- app.js
+++ app.js
@@ -9,3 +9,77 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"))
 mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
+
+//CREATE SCHEMA FOR COLLECTION
+const articleSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+//ARTICLE MODEL
+const Article = mongoose.model("Article", articleSchema);
+
+//NEW ARTICLE
+const article1 = new Article({
+    title: "Test Article",
+    content: "This is a demo article"
+});
+
+//GET ALL ARTICLES
+app.get(
+    "/articles",
+    function(request, response){
+        Article.find({})
+            .then(function(articles){
+                //RETURN THE ARTICLES
+                response.send(articles);
+                
+            })
+            .catch(function(err){
+                if(err){
+                    //RETURN THE ERROR STATEMENT
+                    response.send(err)
+                }
+            });
+});
+
+//DELETE ALL ARTICLES
+app.delete(
+    "/articles",
+    function(request, response){
+        Article.deleteMany({})
+            .then(function(){
+                response.send("Successfully Deleted All Articles");
+            })
+            .catch(function(err){
+                response.send(err);
+            });
+})
+
+//POST A NEW ARTICLE
+app.post(
+    "/articles",
+    function(request, response){
+        console.log(request.body.title);
+        console.log(request.body.content);
+        //API WITHOUT THE NEED OF A FRONTEND GETS THE DATA FROM USER
+        const newArticle = new Article({
+            title: request.body.title,
+            content: request.body.content
+        });
+
+        //SAVE INTO DATABASE
+        newArticle.save()
+        .then(function(){
+            response.send("Successfully saved new article");
+        })
+        .catch(function(err){
+            response.send(err);
+        });
+           
+})
+
+//SETUP SERVER
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
